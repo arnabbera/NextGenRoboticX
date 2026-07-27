e44c8d578cc8e6bbcc242793aa344ba279622810
arnabbera <beraarnab@gmail.com>
2026-07-27 09:05:38 +0530

    Fix GitHub Pages base path

--- vite.config - Copy.js
+++ vite.config - Copy.js
@@ -2,10 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/NextGenRoboticX_V2/" : "/",
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-}));
+});
