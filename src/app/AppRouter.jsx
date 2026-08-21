e50406ac79fa726290f0658de8b8e16843cb4973
arnabbera <36722756+arnabbera@users.noreply.github.com>
2026-08-21 21:23:01 +0530

    Register admin course and project routes

--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -1,27 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ProtectedRoute from "../components/ProtectedRoute";
-
-// Public Pages
 import HomePage from "../pages/HomePage";
 import NotFoundPage from "../pages/NotFoundPage";
 import FeatureDetailPage from "../features/marketing/pages/FeatureDetailPage";
-
-// Layout
 import Layout from "../components/layout/Layout";
-
-// Auth
 import LoginPage from "../features/auth/pages/LoginPage";
-
-// Dashboard
 import Dashboard from "../features/dashboard/pages/Dashboard";
-
-// Courses
 import Courses from "../features/courses/pages/Courses";
 import CourseDetails from "../features/courses/pages/CourseDetails";
 import RoboticsTestPage from "../features/assessment/pages/RoboticsTestPage";
-
-// Learning
 import LearningPage from "../features/learning/pages/LearningPage";
 import ArduinoBasicsChapterPage from "../features/learning/pages/ArduinoBasicsChapterPage";
 import SensorsActuatorsChapterPage from "../features/learning/pages/SensorsActuatorsChapterPage";
@@ -32,8 +20,6 @@ import LineFollowingRobotChapterPage from "../features/learning/pages/LineFollow
 import VoiceControlledRobotChapterPage from "../features/learning/pages/VoiceControlledRobotChapterPage";
 import AIRobotIntegrationChapterPage from "../features/learning/pages/AIRobotIntegrationChapterPage";
 import FinalProjectChapterPage from "../features/learning/pages/FinalProjectChapterPage";
-
-// Student Pages
 import Projects from "../features/projects/pages/Projects";
 import Certificates from "../features/projects/pages/Certificates";
 import Profile from "../features/projects/pages/Profile";
@@ -48,80 +34,32 @@ import FaceRecognitionRobotPage from "../features/projects/pages/FaceRecognition
 import HumanoidRobotPage from "../features/projects/pages/HumanoidRobotPage";
 import RoboticArmAutomationPage from "../features/projects/pages/RoboticArmAutomationPage";
 import SmartAgriculturePage from "../features/projects/pages/SmartAgriculturePage";
-
-// Admin
 import AdminRoute from "../components/auth/AdminRoute";
 import AdminLayout from "../layouts/AdminLayout";
-
 import AdminDashboard from "../features/admin/pages/AdminDashboard";
 import CourseManagement from "../features/admin/pages/CourseManagement";
 import CourseEditor from "../features/admin/pages/CourseEditor";
+import ProjectManagement from "../features/admin/pages/ProjectManagement";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* ===================== */}
-        {/* Public Routes */}
-        {/* ===================== */}
-
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/features/:featureSlug"
-          element={<FeatureDetailPage />}
-        />
+        <Route path="/features/:featureSlug" element={<FeatureDetailPage />} />
 
         <Route element={<ViewOnlyProjectRoute />}>
-          <Route
-            path="/projects/obstacle-avoiding-robot"
-            element={<ObstacleAvoidingRobotPage />}
-          />
-
-          <Route
-            path="/projects/line-following-robot"
-            element={<LineFollowingRobotPage />}
-          />
-
-          <Route
-            path="/projects/iot-smart-monitoring"
-            element={<IoTSmartMonitoringPage />}
-          />
-
-          <Route
-            path="/projects/smart-home-automation"
-            element={<SmartHomeAutomationPage />}
-          />
-
-          <Route
-            path="/projects/arduino-drone"
-            element={<ArduinoDronePage />}
-          />
-
-          <Route
-            path="/projects/face-recognition-robot"
-            element={<FaceRecognitionRobotPage />}
-          />
-
-          <Route
-            path="/projects/humanoid-robot"
-            element={<HumanoidRobotPage />}
-          />
-
-          <Route
-            path="/projects/robotic-arm-automation"
-            element={<RoboticArmAutomationPage />}
-          />
-
-          <Route
-            path="/projects/smart-agriculture"
-            element={<SmartAgriculturePage />}
-          />
+          <Route path="/projects/obstacle-avoiding-robot" element={<ObstacleAvoidingRobotPage />} />
+          <Route path="/projects/line-following-robot" element={<LineFollowingRobotPage />} />
+          <Route path="/projects/iot-smart-monitoring" element={<IoTSmartMonitoringPage />} />
+          <Route path="/projects/smart-home-automation" element={<SmartHomeAutomationPage />} />
+          <Route path="/projects/arduino-drone" element={<ArduinoDronePage />} />
+          <Route path="/projects/face-recognition-robot" element={<FaceRecognitionRobotPage />} />
+          <Route path="/projects/humanoid-robot" element={<HumanoidRobotPage />} />
+          <Route path="/projects/robotic-arm-automation" element={<RoboticArmAutomationPage />} />
+          <Route path="/projects/smart-agriculture" element={<SmartAgriculturePage />} />
         </Route>
-
-        {/* ===================== */}
-        {/* Protected Routes */}
-        {/* ===================== */}
 
         <Route element={<ProtectedRoute />}>
           <Route element={<Layout />}>
@@ -129,50 +67,17 @@ export default function AppRouter() {
             <Route path="/courses" element={<Courses />} />
             <Route path="/courses/:courseId" element={<CourseDetails />} />
             <Route path="/courses/:courseId/learn" element={<LearningPage />} />
-            <Route
-              path="/courses/robotics-foundation/mock-test"
-              element={<RoboticsTestPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/assessment"
-              element={<RoboticsTestPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-2"
-              element={<ArduinoBasicsChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-3"
-              element={<SensorsActuatorsChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-4"
-              element={<L298NMotorDriverChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-5"
-              element={<BluetoothRobotChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-6"
-              element={<ObstacleAvoidingRobotChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-7"
-              element={<LineFollowingRobotChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-8"
-              element={<VoiceControlledRobotChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-9"
-              element={<AIRobotIntegrationChapterPage />}
-            />
-            <Route
-              path="/courses/robotics-foundation/learn/chapter-10"
-              element={<FinalProjectChapterPage />}
-            />
+            <Route path="/courses/robotics-foundation/mock-test" element={<RoboticsTestPage />} />
+            <Route path="/courses/robotics-foundation/assessment" element={<RoboticsTestPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-2" element={<ArduinoBasicsChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-3" element={<SensorsActuatorsChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-4" element={<L298NMotorDriverChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-5" element={<BluetoothRobotChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-6" element={<ObstacleAvoidingRobotChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-7" element={<LineFollowingRobotChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-8" element={<VoiceControlledRobotChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-9" element={<AIRobotIntegrationChapterPage />} />
+            <Route path="/courses/robotics-foundation/learn/chapter-10" element={<FinalProjectChapterPage />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/certificates" element={<Certificates />} />
             <Route path="/profile" element={<Profile />} />
@@ -180,9 +85,19 @@ export default function AppRouter() {
           </Route>
         </Route>
 
-        {/* ===================== */}
-        {/* 404 */}
-        {/* ===================== */}
+        <Route
+          element={
+            <AdminRoute>
+              <AdminLayout />
+            </AdminRoute>
+          }
+        >
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/courses" element={<CourseManagement />} />
+          <Route path="/admin/courses/new" element={<CourseEditor />} />
+          <Route path="/admin/courses/:courseId/edit" element={<CourseEditor />} />
+          <Route path="/admin/projects" element={<ProjectManagement />} />
+        </Route>
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
