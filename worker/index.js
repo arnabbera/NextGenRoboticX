2cc171c7d1af1614e500c0adcac4fc728880fea5
arnabbera <36722756+arnabbera@users.noreply.github.com>
2026-09-15 21:22:56 +0530

    Add IoT certification scoring

--- worker/index.js
+++ worker/index.js
@@ -603,6 +603,13 @@ const CERTIFICATION_CONFIG = {
       assessment2: Array.from({ length: 50 }, (_, index) => (4 - ((index + 2) % 4)) % 4),
     },
   },
+  "internet-of-things": {
+    title: "Internet of Things", prefix: "IOT",
+    answerKeys: {
+      assessment1: Array.from({ length: 50 }, (_, index) => (4 - ((index + 1) % 4)) % 4),
+      assessment2: Array.from({ length: 50 }, (_, index) => (4 - ((index + 2) % 4)) % 4),
+    },
+  },
 };
 
 function certificationKey(courseId, uid) {
